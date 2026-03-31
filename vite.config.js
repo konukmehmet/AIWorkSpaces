d2647659e8bcca5beb3f89afbff5fb800a17c9cc
Konuk <mehmet11.konuk@bs.nttdata.com>
2026-03-31 15:48:44 +0300

    chore: add deployment workflow

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/AIWorkSpaces/', // Sets base path for GitHub Pages matching the repository name
+})
